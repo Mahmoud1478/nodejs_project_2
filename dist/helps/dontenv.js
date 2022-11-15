88c42fcf102b2be1f76765de663a09b0d91db9c2
mahmoud1478 <mah.mostafa18@gmail.com>
2022-11-15 06:22:54 +0200

    gitignore

--- dist/helps/dontenv.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-var env = function (key) {
-    return process.env[key];
-};
-exports.env = env;
